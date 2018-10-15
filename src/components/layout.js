d462c2d791fc63b7a30ff3643f73769eaeb44f86
Steve Wright <ste.c.wr@gmail.com>
2018-10-15 17:08:45 +0100

    working on scrolling navbar changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './header'
 import './layout.css'
 
 const Layout = ({ children }) => (
@@ -18,28 +17,9 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
-      </>
+    <div>
+      {children}
+    </div>
     )}
   />
 )
@@ -48,4 +28,25 @@ Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
 
-export default Layout
+class LayoutComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleScroll = this.handleScroll.bind(this);
+    }
+
+    handleScroll() {
+        console.log("scrolling");
+    }
+
+    render() {
+        return (
+            <div onScroll={this.handleScroll}>
+                <Layout children={this.props.children} />
+            </div>
+        )
+    }
+
+}
+
+export default LayoutComponent
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,92 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+import './index.css'
+
+const Navbar = ({colorAttr}) => (
+    <div style={colorAttr} className="navbar-style">
+        <p>This is the header</p>
+    </div>
 )
+
+class ContentBlock extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.positionRef = React.createRef();
+
+        this._handleScroll = this._handleScroll.bind(this);
+
+        this.setElementRef = element => {
+            this.positionRef = element;
+        };
+    }
+
+    _handleScroll() {
+        console.log(this.positionRef);
+    }
+
+/*    componentDidMount() {
+        window.addEventListener('scroll', this._handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this._handleScroll);
+    } */
+
+    render() {
+        return (
+            <div ref={this.setElementRef} style={{backgroundColor: this.props.backCol}} className="content-block"></div>
+        )
+    }
+
+}
+
+class IndexPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            color: "",
+            bgColor: "red"
+        };
+    }
+
+    setNewColor(e) {
+        this.setState({
+            bgColor: this.state.color
+        });
+        e.preventDefault();
+    }
+
+    render() {
+        var styleVal = {
+            backgroundColor: this.state.bgColor
+        };
+
+        return (
+            <Layout>
+                <Navbar colorAttr={styleVal} />
+                <ContentBlock backCol={'green'} />
+                <ContentBlock backCol={'orange'} />
+                <ContentBlock backCol={'silver'} />
+                <ContentBlock backCol={'blue'} />
+                <ContentBlock backCol={'grey'} />
+                <ContentBlock backCol={'green'} />
+                <ContentBlock backCol={'orange'} />
+                <ContentBlock backCol={'silver'} />
+                <ContentBlock backCol={'blue'} />
+                <ContentBlock backCol={'grey'} />
+                <ContentBlock backCol={'green'} />
+                <ContentBlock backCol={'orange'} />
+                <ContentBlock backCol={'silver'} />
+                <ContentBlock backCol={'blue'} />
+                <ContentBlock backCol={'grey'} />
+            </Layout>
+        )
+    }
+}
 
 export default IndexPage
